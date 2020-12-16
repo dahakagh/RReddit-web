76ce79a384698b56462ed14aded6b2453cdfccab
dahakagh <Marshev123@yandex.ru>
2020-12-16 13:06:10 +0300

    login and register next links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
-const Index = () => <div>Hello World!</div>;
+import { Navbar } from "../components/Navbar";
+
+const Index = () => (
+  <>
+    <Navbar />
+    <div>Hello world</div>
+  </>
+);
 
 export default Index;
